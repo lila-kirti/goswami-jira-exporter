33ff8689444adf7af005845c9ef343bc419689bb
lgalimova <dizzycome@gmail.com>
2020-09-12 14:04:21 +0300

    nf user migrator

--- src/main/java/ru/bvg/CollectionYearGenerator.java
+++ src/main/java/ru/bvg/CollectionYearGenerator.java
@@ -20,20 +20,20 @@ public class CollectionYearGenerator {
             writer.write(generateCollectionYearMain());
             writer.newLine();
 
-            for (int i = 2019; i > 1991; i--) {
+            for (int i = 2020; i > 1991; i--) {
                 writer.write(generateCollectionYear(i));
                 writer.newLine();
             }
             writer.newLine();
 
             int j = 50;
-            for (int i = 2019; i > 1991; i--) {
+            for (int i = 2020; i > 1991; i--) {
                 writer.write(generateCollectionYearHierarchy(i, j++));
                 writer.newLine();
             }
             writer.newLine();
 
-            for (int i = 2019; i > 1991; i--) {
+            for (int i = 2020; i > 1991; i--) {
                 for (String folder : generateCollectionFolders(i)) {
                     writer.write(folder);
                     writer.newLine();
@@ -67,11 +67,12 @@ public class CollectionYearGenerator {
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Все лекции'), 1);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Семинары'), 2);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Паломничества'), 3);", year, year));
-        list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Обращения, встречи с учениками'), 4);", year, year));
+        list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Встречи с учениками'), 4);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Публичные лекции'), 5);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Лекции на праздниках'), 6);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Лекции на церемонии посвящения'), 7);", year, year));
         list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Ретриты и фестивали'), 8);", year, year));
+        list.add(String.format("INSERT INTO collection_hierarchy (parent_id, children_id, ordern) VALUES ((select id from collection where full_name='%d год'), (select id from collection where full_name='%d. Обращения'), 9);", year, year));
         return list;
     }
 
@@ -80,12 +81,12 @@ public class CollectionYearGenerator {
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, order_by, direction)  VALUES ('Все', '%d. Все лекции', 'filter', 'collection/all.jpg', '%d-01-01', '%d-12-31', 'date', 'ASC');", year, year, year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url)  VALUES ('Семинары', '%d. Семинары', 'collection', 'collection/seminar.jpg');", year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Паломничества', '%d. Паломничества', 'filter', 'collection/parikram.jpg', '%d-01-01', '%d-12-31', 1, 'date', 'ASC');", year, year, year));
-        folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Обращения, встречи с учениками', '%d. Обращения, встречи с учениками', 'filter', 'collection/treatment.jpg', '%d-01-01', '%d-12-31', 5, 'date', 'ASC');", year, year, year));
-        folders.add(String.format("INSERT INTO collection_filter_tag (collection_id, tag_id)  VALUES ((select id from collection where full_name='%d. Обращения, встречи с учениками'), (select id from tag where name='встречи с учениками'));", year));
+        folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Встречи с учениками', '%d. Обращения, встречи с учениками', 'filter', 'collection/treatment.jpg', '%d-01-01', '%d-12-31', 5, 'date', 'ASC');", year, year, year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Публичные лекции', '%d. Публичные лекции', 'filter', 'collection/public.jpg', '%d-01-01', '%d-12-31', 4, 'date', 'ASC');", year, year, year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Праздники', '%d. Лекции на праздниках', 'filter', 'collection/celebration.jpg', '%d-01-01', '%d-12-31', 2, 'date', 'ASC');", year, year, year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Инициация', '%d. Лекции на церемонии посвящения', 'filter', 'collection/initiation.jpg', '%d-01-01', '%d-12-31', 7, 'date', 'ASC');", year, year, year));
         folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url)  VALUES ('Ретриты и фестивали', '%d. Ретриты и фестивали', 'collection', 'collection/9ef81568-b905-4c02-ba24-bb2e05e35329.jpg');", year));
+        folders.add(String.format("INSERT INTO collection (short_name, full_name, source, img_url, date_from, date_to, category_id, order_by, direction)  VALUES ('Обращения', '%d. Обращения', 'filter', 'collection/treatment.jpg', '%d-01-01', '%d-12-31', 3, 'date', 'ASC');", year, year, year));
         return folders;
     }
 }
---
+++ src/main/resources/sql/2.refbooks_english.sql
@@ -0,0 +1,39 @@
+insert into scripture (id, name,description) values(1, 'Бхагавад-гита',
+'Укажите главу и номер стиха для поиска. Примеры:<br/>
+<b>1</b> (1 глава)<br/>
+<b>1-3</b> (с 1 по 3 главы)<br/>
+<b>1.4</b> (1 глава, 4 стих)<br/>
+<b>1.4-7</b> (1 глава, с 4 по 7 стихи)<br/>');
+insert into scripture (id, name,description) values(2, 'Шримад-бхагаватам',
+'Укажите песнь, главу и номер стиха для поиска. Примеры:<br/>
+<b>1</b> (1 песнь)<br/>
+<b>1.2</b> (1 песнь, 2 глава)<br/>
+<b>1.2-4</b> (1 песнь, с 2 по 4 главы)<br/>
+<b>1.2.1-10</b> (1 песнь, 2 глава, с 1 по 10 стихи)<br/>');
+insert into scripture (id, name,description) values(3, 'Чайтанья-чаритамрита',
+'Укажите том, главу и номер стиха для поиска. Примеры:<br/>
+<b>Ади</b> (Ади-лила, варианты: Мадхья, Антья)<br/>
+<b>Ади 2</b> (Ади-лила, 2 глава)<br/>
+<b>Ади 2-3</b> (Ади-лила, с 2 по 3 главы)<br/>
+<b>Ади 2.4</b> (Ади-лила, 2 глава, 4 стих)<br/>
+<b>Ади 2.115-118</b> (Ади-лила, 2 глава, с 115 по 118 стихи)<br/>');
+insert into scripture (id, name,description) values(4, 'Нектар преданности','Укажите номер главы для поиска');
+insert into scripture (id, name,description) values(5, 'Нектар наставлений','Укажите номер стиха для поиска');
+insert into scripture (id, name,description) values(6, 'Шри Ишопанишад','Укажите номер стиха для поиска');
+insert into scripture (id, name) values(7, 'Мадхурья-кадмбини');
+insert into scripture (id, name) values(8, 'Рамаяна');
+
+insert into category (id, name) values(1, 'Паломничество');
+insert into category (id, name) values(2, 'Праздник');
+insert into category (id, name) values(3, 'Обращение');
+insert into category (id, name) values(4, 'Публичная лекция');
+insert into category (id, name) values(5, 'Встреча с учениками');
+insert into category (id, name) values(6, 'Киртан');
+insert into category (id, name) values(7, 'Инициация');
+insert into category (id, name) values(8, 'Обсуждение');
+
+insert into role (id, name) values(1, 'Пользователь');
+insert into role (id, name) values(2, 'Администратор');
+insert into role (id, name) values(3, 'Редактор');
+insert into role (id, name) values(4, 'Представитель');
+
